2befe1e9ff21591dbe75406122b66ef3b1edf4fc
andre.campopiano <andre.campopiano@cast.com.br>
2016-03-09 16:04:00 -0300

    Finalizando Formulario em HTML

--- WebView/app/src/main/java/dominando/android/webview/MainActivity.java
+++ WebView/app/src/main/java/dominando/android/webview/MainActivity.java
@@ -2,25 +2,62 @@ package dominando.android.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView mWebView;
+
+
+/* @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.loadUrl("http://www.nglauber.com.br");
-        mWebView.setWebViewClient(new WebViewClient(){
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(this,"dominando");
+        mWebView.loadUrl("file:///android_asset/meu.html");
+
+        /*mWebView.loadUrl("http://www.nglauber.com.br");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                //Começou a carregar a pagina
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return false;
             }
-        });
+        });*/
+
     }
+
+    @JavascriptInterface
+    public void showToast(String s, String t){
+        Toast.makeText(this, "Nome: " + s + " Idade: " + t, Toast.LENGTH_SHORT).show();
+    }
+
 }
