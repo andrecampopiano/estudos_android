66db9741844d7999435a22bf296353560b2b643b
andrecampopiano <brocoyou@hotmail.com>
2016-03-08 21:00:53 -0300

    Criação do WebView

--- WebView/app/src/main/java/dominando/android/webview/MainActivity.java
+++ WebView/app/src/main/java/dominando/android/webview/MainActivity.java
@@ -2,12 +2,25 @@ package dominando.android.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.loadUrl("http://www.nglauber.com.br");
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
     }
 }
