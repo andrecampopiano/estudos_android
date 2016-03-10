10dfe2f3a867006e98861ab87b5cb9dba3965975
andrecampopiano <brocoyou@hotmail.com>
2016-03-10 10:54:16 -0300

    Inclusao da imagem x e y e atualização da class JogoDaVelhaView.java

--- CustomView/app/src/main/java/dominando/android/customview/JogoDaVelhaView.java
+++ CustomView/app/src/main/java/dominando/android/customview/JogoDaVelhaView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -33,13 +36,13 @@ public class JogoDaVelhaView extends View {
     }
 
     @Override
-    public void onAttachedToWindow(){
+    public void onAttachedToWindow() {
         super.onAttachedToWindow();
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
 
-      /*  mImageX = BitmapFactory.decodeResource(getResources(), R.drawable.x_mark);
-        mImageO = BitmapFactory.decodeResource(getResources(), R.drawable.o_mark);*/
+        mImageX = BitmapFactory.decodeResource(getResources(), R.drawable.x_mark);
+        mImageO = BitmapFactory.decodeResource(getResources(), R.drawable.o_mark);
 
     }
 
@@ -57,16 +60,51 @@ public class JogoDaVelhaView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        if(getLayoutParams().width == ViewGroup.LayoutParams.WRAP_CONTENT){
+        if (getLayoutParams().width == ViewGroup.LayoutParams.WRAP_CONTENT) {
             Resources r = getResources();
-            float quadrante = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48 ,r.getDisplayMetrics());
+            float quadrante = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, r.getDisplayMetrics());
             mTamanho = (int) quadrante * 3;
-        }else if (getLayoutParams().width == ViewGroup.LayoutParams.MATCH_PARENT){
+        } else if (getLayoutParams().width == ViewGroup.LayoutParams.MATCH_PARENT) {
             mTamanho = Math.min(MeasureSpec.getSize(widthMeasureSpec),
                     MeasureSpec.getSize(heightMeasureSpec));
-        }else{
+        } else {
             mTamanho = getLayoutParams().width;
         }
         setMeasuredDimension(mTamanho, mTamanho);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int quadrante = mTamanho / 3;
+
+        //Desenhando as linhas
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(3);
+
+        //Verticais
+        canvas.drawLine(quadrante, 0, quadrante, mTamanho, mPaint);
+        canvas.drawLine(quadrante * 2, 0, quadrante * 2, mTamanho, mPaint);
+
+        //Horizontais
+        canvas.drawLine(0, quadrante, mTamanho, quadrante, mPaint);
+        canvas.drawLine(0, quadrante * 2, mTamanho, quadrante * 2, mPaint);
+
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+
+                int x = coluna * quadrante;
+                int y = linha * quadrante;
+
+                Rect rect =  new Rect(x,y,x + quadrante, y + quadrante);
+
+                if(mTabuleiro[linha][coluna] == XIS){
+                    canvas.drawBitmap(mImageX, null , rect , null);
+                }else if (mTabuleiro[linha][coluna] == BOLA){
+                    canvas.drawBitmap(mImageO, null, rect, null);
+                }
+            }
+        }
+
     }
 }
